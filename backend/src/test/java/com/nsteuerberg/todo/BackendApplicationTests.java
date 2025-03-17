27e44bd6b45eff2e8968438c8f8ef61e1389e417
NSteuerberg <nicolassteuerberg@gmail.com>
2025-03-17 20:17:50 +0100

    Backend:: init project

---
+++ backend/src/test/java/com/nsteuerberg/todo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsteuerberg.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
