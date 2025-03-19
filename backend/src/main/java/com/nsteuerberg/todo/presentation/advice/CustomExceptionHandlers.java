d22c76f98a1d78cf6242a692d8999b5fbdfabc5e
NSteuerberg <nicolassteuerberg@gmail.com>
2025-03-19 20:31:04 +0100

    BACKEND:: Excepcion validacion + TaskNotFound desplazada a handler custom

---
+++ backend/src/main/java/com/nsteuerberg/todo/presentation/advice/CustomExceptionHandlers.java
@@ -0,0 +1,17 @@
+package com.nsteuerberg.todo.presentation.advice;
+
+import com.nsteuerberg.todo.service.exception.TaskNotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandlers {
+
+    @ExceptionHandler(TaskNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleTaskNotFound(TaskNotFound taskNotFound) {
+        return taskNotFound.getMessage();
+    }
+}
--- backend/src/main/java/com/nsteuerberg/todo/presentation/advice/GlobalExceptionHandler.java
+++ backend/src/main/java/com/nsteuerberg/todo/presentation/advice/GlobalExceptionHandler.java
@@ -1,18 +1,42 @@
 package com.nsteuerberg.todo.presentation.advice;
 
 import com.nsteuerberg.todo.service.exception.TaskNotFound;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(TaskNotFound.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleTaskNotFound(TaskNotFound taskNotFound) {
-        return taskNotFound.getMessage();
+    @Getter
+    @AllArgsConstructor
+    public class ErrorResponse {
+        private String message;
+        private Object errors;
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationError(MethodArgumentNotValidException illegalArguments) {
+        // recogemos los errores
+        List<String> errors = illegalArguments.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ErrorResponse(
+                "Validación incorrecta, revisa los errores para saber que datos fallan o faltan",
+                errors
+        );
+    }
+
 }
